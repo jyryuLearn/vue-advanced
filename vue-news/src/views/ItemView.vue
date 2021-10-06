59a0e76e2cffbf0ce7cfd2b447d63ce58dd25e0a
jeongyy <jeongyy@gmail.com>
2021-10-06 19:37:05 +0900

    라우터 실전까지.

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,18 +1,37 @@
 <template>
 	<div>
-		<p> {{ itemInfo.title }}</p>
-		<div> {{ itemInfo.content }}</div>
+		<section>
+<!--			ask desc-->
+			<div class="user-container">
+				<div>
+					<i class="fas fa-user"></i>
+				</div>
+				<div class="user-desc">
+					<router-link :to="`/user/${fetchedItem.user}`">
+						{{ fetchedItem.user }}
+					</router-link>
+					<div class="time">
+						{{ fetchedItem.time_ago }}
+					</div>
+				</div>
+			</div>
+			<h2>{{ fetchedItem.title }}</h2>
+		</section>
+		<section>
+<!--			reply-->
+			<div v-html="fetchedItem.content"> </div>
+		</section>
 	</div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
 	name: "ItemView.vue",
 
 	computed: {
-		itemInfo() {
-			return this.$store.state.item;
-		}
+		...mapGetters(['fetchedItem']),
 	},
 
 	created() {
@@ -23,5 +42,18 @@ export default {
 </script>
 
 <style scoped>
-
+.user-container {
+	display: flex;
+	align-items: center;
+	padding: 0.5rem;
+}
+.fa-user {
+	font-size: 2.5rem;
+}
+.user-desc {
+	padding-left: 6px;
+}
+.time {
+	font-size: 0.7rem;
+}
 </style>
